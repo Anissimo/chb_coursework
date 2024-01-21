044b69686bae5035d17e9510a7a29ea6467e9f6e
Анастасия Ванькова <grisaanisimov@gmail.com>
2024-01-21 15:34:59 +0300

    задал структуру проекта

--- src/config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-
-MONGODB_SERVER = 'mongodb://localhost:27017'
-TOKEN = '6359995047:AAE-KVnGBBZgYtfA8gVYuq4xHN5G-4w_Azc'
---
+++ src/modules/config.py
@@ -0,0 +1,7 @@
+"""
+Файл конфигурации. Этот файл содержит конфигурационные параметры, такие как токен бота и параметры подключения к базе данных.
+"""
+
+BOT_TOKEN = '6844147281:AAF1cSp1Ca7SJvm8A8QpfV6HjWmw-svNn4E'
+DB_HOST = 'YOUR_DB_HOST_HERE'
+DB_PORT = 'YOUR_DB_PORT_HERE'
