840f6c9d122eea3d3fb6c111f7713835d785ee1b
grisha <grisaanisimov@gmail.com>
2024-01-22 09:29:05 +0400

    накинутуа основная логика работы

--- src/main.py
+++ src/main.py
@@ -4,16 +4,29 @@
 """
 
 import telebot
-from modules import config, command_handler, database_handler, nlp_handler, user_interface
+from telebot import types
+
+from .modules import config, command_handler, database_handler, nlp_handler, user_interface
+
+# main.py
+import telebot
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот магазина компьютерных комплектующих. Чем я могу помочь?")
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    itembtn1 = types.KeyboardButton('Привет')
+    itembtn2 = types.KeyboardButton('Помощь')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(message.chat.id, "Привет! Я бот магазина компьютерных комплектующих. Чем я могу помочь?", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: message.text == 'Привет')
+def greet(message):
+    bot.reply_to(message, "Привет! Чем я могу помочь?")
+
+@bot.message_handler(func=lambda message: message.text == 'Помощь')
+def help(message):
+    command_handler.handle_help_button(bot, message)
 
 bot.polling()
--- src/modules/command_handler.py
+++ src/modules/command_handler.py
@@ -2,8 +2,21 @@
 Модуль обработки команд. Этот модуль содержит функции для обработки команд от пользователей.
 """
 
-def handle_command(command):
-    """
-    Обрабатывает команду от пользователя.
-    """
-    # Ваш код здесь
+# command_handler.py
+from telebot import types
+# from modules import database_handler
+
+def handle_help_button(bot, message):
+    markup = types.InlineKeyboardMarkup()
+    itembtn1 = types.InlineKeyboardButton('Информация о конкретном магазине', callback_data='shop_info')
+    itembtn2 = types.InlineKeyboardButton('Информация о всех магазинах', callback_data='all_shops')
+    itembtn3 = types.InlineKeyboardButton('Товары в конкретном магазине', callback_data='shop_products')
+    itembtn4 = types.InlineKeyboardButton('Все магазины определённого бренда', callback_data='brand_shops')
+    itembtn5 = types.InlineKeyboardButton('Поиск', callback_data='search')
+
+    markup.add(itembtn1)
+    markup.add(itembtn2)
+    markup.add(itembtn3)
+    markup.add(itembtn4)
+    markup.add(itembtn5)
+    bot.send_message(message.chat.id, "Выберите одну из следующих опций:", reply_markup=markup)
