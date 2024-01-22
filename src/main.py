50f2f6a57b134c0997339c32786565a46ab7fee3
Анастасия Ванькова <grisaanisimov@gmail.com>
2024-01-22 15:50:20 +0300

    сегодня

--- src/main.py
+++ src/main.py
@@ -1,17 +1,12 @@
-"""
-Главный файл бота. Этот файл служит точкой входа для бота.
-Он импортирует и использует модули из каталога 'modules'.
-"""
-
+# main.py
+# main.py
 import telebot
 from telebot import types
 
-from .modules import config, command_handler, database_handler, nlp_handler, user_interface
-
-# main.py
-import telebot
+import config, command_handler, database_handler, nlp_handler, user_interface
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
+user_interface.set_bot(bot)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -28,5 +23,16 @@ def greet(message):
 @bot.message_handler(func=lambda message: message.text == 'Помощь')
 def help(message):
     command_handler.handle_help_button(bot, message)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'shop_info':
+        user_interface.ask_for_shop(bot, call.message)
+    elif call.data == 'all_shops':
+        user_interface.send_all_shops(bot, call.message)
+    elif call.data == 'shop_products':
+        user_interface.ask_for_shop(bot, call.message)
+    elif call.data == 'brand_shops':
+        user_interface.ask_for_shop(bot, call.message)
 
 bot.polling()
